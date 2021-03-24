bccdd86b7a51c38f30869ffa00b311fa49d47e88
AntoninJuillard <60604725+AntoninJuillard@users.noreply.github.com>
2021-03-24 15:43:33 +0100

    Update v.php

--- v.php
+++ v.php
@@ -6,14 +6,36 @@ $userpassword = 'root';
 
 $connect = new mysqli($servername, $username, $userpassword);
 
-$connect->query($sql);
+// verifier la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  //echo "Connected successfully";
 
 // se connecter à la base de données
 $conn = new mysqli($servername, $username, $userpassword, 'urldb');
 
-$sql = "SELECT * FROM urlsystem ";
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    //echo "connecté!";
+}
 
+// afficher quelque chose en fonction de la key dans l'url
 // source aide : https://www.php.net/manual/fr/reserved.variables.get.php 
-$rediriger = $_GET["oui"];
-echo $rediriger;
+//$rediriger = $_GET["key"];
+// echo $rediriger;
+
+// source pour parse_url : https://www.php.net/manual/fr/function.parse-url.php
+
+$url = 'http://localhost:8888/shortener/v.php?key=uI29_EN';
+
+$key = $_GET["key"];
+echo $key;
+// recuperer ce qu'il y a après le '?' dans l'url
+$urlparsequery = parse_url($url, PHP_URL_QUERY);
+settype($urlparsequery, "string");
+echo $urlparsequery;
+
+
 ?>
