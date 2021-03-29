c178be46d5166fc98e6ba37c834704bb72a85a59
Sushi <misaiphonraphael@gmail.com>
2021-03-29 17:52:07 +0200

    registration.css finito

--- registration.php
+++ registration.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>shortener</title>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;600&family=Nunito:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="registration.css">
 </head>
 <body>
