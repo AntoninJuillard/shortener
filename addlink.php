39b9122826b292024c194b022723f29114927dc6
AntoninJuillard <60604725+AntoninJuillard@users.noreply.github.com>
2021-03-30 23:30:24 +0200

    update

--- addlink.php
+++ addlink.php
@@ -175,7 +175,9 @@ if(isset($_GET['change']))
     </div>
     <div class="account_zone">
         <div class="account_container">
-        <div class="account_container_close-button"></div>
+        <div class="account_container_close-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z"/></svg>
+        </div>
             <div class="account_link-title">My URLs</div>
             <div class="account_link-container">
                 <?php while($rows = mysqli_fetch_row($sessionresult)) { ?>
