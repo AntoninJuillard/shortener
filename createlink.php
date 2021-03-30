f218fed9b90d7e0dd3344fc5f4113483e846d4c1
AntoninJuillard <60604725+AntoninJuillard@users.noreply.github.com>
2021-03-30 19:37:37 +0200

    small changes and trying to organise components

--- createlink.php
+++ createlink.php
@@ -161,10 +161,10 @@ if(isset($_GET['change']))
                         <!-- change the display of the button depending on whether the link is enabled or not -->
                         <?php if($rows[3]==='true') { ?>
                             <?php echo '<div class="account_link-element_state"></div>'; ?>
-                            <?php echo '<a href=addlink.php?change='.$rows[0].'>desactiver</a>'; ?>
+                            <?php echo '<a href=createlink.php?change='.$rows[0].'>desactiver</a>'; ?>
                         <?php } else { ?>
                             <?php echo '<div class="account_link-element_stateoff"></div>'; ?>
-                            <?php echo '<a href=addlink.php?change='.$rows[0].'>activer</a>'; ?>
+                            <?php echo '<a href=createlink.php?change='.$rows[0].'>activer</a>'; ?>
                         <?php }; ?>
                             
                         <?php echo '<div class="account_link-element_views">'; ?>
